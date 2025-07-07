b8cdd3e9d5a4ffd5dc2b6007bb6865e77e23f066
lakshay17 <lakshayjain01lj@gmail.com>
2025-07-07 20:15:56 +0530

    v1

--- server/index.js
+++ server/index.js
@@ -110,11 +110,10 @@ app.get('/chat', async (req, res) => {
 
 })
 
-
-
-app.listen(8000, () => {
-    console.log(`Server started at port ${8000}`)
-})
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+});
 
 
 /*
