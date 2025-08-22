a981edbe43e3d9e3c5f36fd6c6594ede7db8b854
lakshay17 <lakshayjain01lj@gmail.com>
2025-08-22 13:10:41 +0530

    deployed be link

--- client/config.js
+++ client/config.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "http://askmypdf-api.eba-faytbgvt.us-east-1.elasticbeanstalk.com/"
+    : "https://askmypdf-backend.onrender.com"
